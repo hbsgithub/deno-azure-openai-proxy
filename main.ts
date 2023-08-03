f3c96d95e32ee17af394e48b9871a10692516df3
hbsgithub <296305184@qq.com>
2023-08-03 15:50:13 +0800

    support gpt-4-32k

--- main.ts
+++ main.ts
@@ -9,7 +9,8 @@ const apiVersion:string = Deno.env.get("API_VERSION");
 const mapper:any = {
   'gpt-3.5-turbo': Deno.env.get("DEPLOY_NAME_GPT35"),
   'gpt-3.5-turbo-16k': Deno.env.get("DEPLOY_NAME_GPT35_16k"),
-  'gpt-4': Deno.env.get("DEPLOY_NAME_GPT4") 
+  'gpt-4': Deno.env.get("DEPLOY_NAME_GPT4"),
+  'gpt-4-32k': Deno.env.get("DEPLOY_NAME_GPT4_32k"),
   // Other mapping rules can be added here.
 };
 
